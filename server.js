56a832b1da5a9c243ed2e97372892af1e700080a
illusion030 <illusion030@gmail.com>
2018-01-15 00:41:55 +0800

    Modify community's adding design

--- server.js
+++ server.js
@@ -96,16 +96,16 @@ app.post("/search", function (req, res) {
 app.post('/renew',function(req, res){
     MongoClient.connect(dbPath, function(err1, db1) {
         if (err1) throw err1;
-        db1.collection("community_info").find({//取得社區資料
+        db1.collection("empty_community").find({//取得社區資料
             community_id: 1
         }).toArray(function(err2, result1){
             if (err2) throw err2
             if (result1 != 0) {
                 var c_date=result1[0].update_time;//community's renew date
                 var c_file=result1[0].community_file;//community's file info
-                var communityX = JSON.parse(result1[0].communityX);
-                var communityY = JSON.parse(result1[0].communityY);
-                var communitymaxY = JSON.parse(result1[0].communitymaxY);
+                var communityX = 0;
+                var communityY = 0;
+                var communitymaxY = 0;
                 var Group_obj = JSON.parse(c_file);
 
                 MongoClient.connect(dbPath, function(err3, db2) {
@@ -114,145 +114,74 @@ app.post('/renew',function(req, res){
                         last_modified_time: {$gte: c_date}//搜尋日期在c_date後的
                     }).toArray(function(err4,result2){
                         if (err4) throw err4;
-                        if (result2 == 0) {//不需要更新社區時
-                            res.send({
-                                file: Group_obj							
-                            })
-                        } else {//需要更新社區時
 
-                            for (i = 0; i < result2.length; ++i) {
+                        for (i = 0; i < result2.length; ++i) {
+                            
+                            var design = JSON.parse(result2[i].design);
+                            var user_name = "user" + result2[i].design_id;
                                 
-                                var design = JSON.parse(result2[i].design);
-                                var user_name = "user" + result2[i].design_id;
+                            var minX = 99999999
+                            var minY = 99999999
+                            var maxX = -99999999
+                            var maxY = -99999999
+
+                            // Calculate house min rect
+                            for (j in design.floorplan.corners) {
+                                if (design.floorplan.corners[j].x < minX)
+                                    minX = design.floorplan.corners[j].x
+                                if (design.floorplan.corners[j].y < minY)
+                                    minY = design.floorplan.corners[j].y
+                                if (design.floorplan.corners[j].x > maxX)
+                                    maxX = design.floorplan.corners[j].x
+                                if (design.floorplan.corners[j].y > maxY)
+                                    maxY = design.floorplan.corners[j].y
+                            }
                                 
-                                var minX = 99999999
-                                var minY = 99999999
-                                var maxX = -99999999
-                                var maxY = -99999999
-                                
-                                for (j in Group_obj.floorplan.corners) {
-                                    if (j.split("_")[0] == user_name)
-                                        delete Group_obj.floorplan.corners[j]
-                                }
+                            var rangeX = minX - communityX
+                            var rangeY = minY - communityY
 
-                                for(j = 0;; j++) {
-                                    if (j == Group_obj.floorplan.walls.length)
-                                        break
-                                    if (Group_obj.floorplan.walls[j].corner1.split("_")[0] == user_name) {
-                                        var deletedWall = Group_obj.floorplan.walls.splice(j, 1)
-                                        j--
-                                    }
-                                }
+                            // Add corners
+                            for (j in design.floorplan.corners)
+                                j = user_name + "_" + j;
 
-                                for (j = 0;; j++) {
-                                    if (j == Group_obj.items.length)
-                                        break
-                                    if (Group_obj.items[j].item_name.split("_")[0] == user_name) {
-                                        var deletedItem = Group_obj.items.splice(j, 1)
-                                        j--
-                                    }
-                                }
-
-                                // Calculate house min rect
-                                for (j in design.floorplan.corners) {
-                                    if (design.floorplan.corners[j].x < minX)
-                                        minX = design.floorplan.corners[j].x
-                                    if (design.floorplan.corners[j].y < minY)
-                                        minY = design.floorplan.corners[j].y
-                                    if (design.floorplan.corners[j].x > maxX)
-                                        maxX = design.floorplan.corners[j].x
-                                    if (design.floorplan.corners[j].y > maxY)
-                                        maxY = design.floorplan.corners[j].y
-                                }
-                                
-                                var rangeX = minX - communityX
-                                var rangeY = minY - communityY
-
-                                // Add corners
-                                for (j in design.floorplan.corners)
-                                    j = user_name + "_" + j;
-
-                                for (j in design.floorplan.corners) {
-                                    Group_obj.floorplan.corners[user_name + "_" + j] = {"x": design.floorplan.corners[j].x - rangeX, "y": design.floorplan.corners[j].y - rangeY}
-                                }
-                                
-                                // Add walls
-                                for (j = 0; j < design.floorplan.walls.length; j++) {
-                                    design.floorplan.walls[j].corner1 = user_name + "_" + design.floorplan.walls[j].corner1
-                                    design.floorplan.walls[j].corner2 = user_name + "_" + design.floorplan.walls[j].corner2
-                                }
-
-                                for (j = 0; j < design.floorplan.walls.length; j++) {
-                                    Group_obj.floorplan.walls.push(design.floorplan.walls[j])
-                                }
-
-                                // Add items
-                                for (j = 0; j < design.items.length; ++j) {
-                                    design.items[j].xpos = design.items[j].xpos - rangeX
-                                    design.items[j].zpos = design.items[j].zpos - rangeY
-                                    design.items[j].item_name = user_name + "_" + design.items[j].item_name
-                                }
-                                for (j = 0; j < design.items.length; ++j) {
-                                    Group_obj.items.push(design.items[j]);
-                                }
-
-                                communityX = communityX + (maxX - minX) + 200
-                                if ((maxY - minY) > communitymaxY)
-                                    communitymaxY = maxY - minY
-
-                                if (communityX > 5000) {
-                                    communityY = communityY + 600 + communitymaxY
-                                    communityX = 0
-
-                                }
+                            for (j in design.floorplan.corners) {
+                                Group_obj.floorplan.corners[user_name + "_" + j] = {"x": design.floorplan.corners[j].x - rangeX, "y": design.floorplan.corners[j].y - rangeY}
+                            }
+                            
+                            // Add walls
+                            for (j = 0; j < design.floorplan.walls.length; j++) {
+                                design.floorplan.walls[j].corner1 = user_name + "_" + design.floorplan.walls[j].corner1
+                                design.floorplan.walls[j].corner2 = user_name + "_" + design.floorplan.walls[j].corner2
                             }
 
-                            res.send({
-                                file: Group_obj
-                            })
-                            
-                            var answer = JSON.stringify(Group_obj);
-                            var myquery = {community_id:1};
-                            var set_community = {$set:{community_file: answer}};
-                            
-                            var time = new Date();
-                            time = time.toJSON();
-                            var set_time = {$set:{update_time: time}};
-                            
-                            var set_xy = {$set:{communityX: communityX, communityY: communityY, communitymaxY: communitymaxY}};
-                            
-                            MongoClient.connect(dbPath, function(err5, db3) {
-                                if (err5) throw err5;
-                                db3.collection("community_info").updateOne(//更新資料庫中community_info的community_file
-                                    myquery,
-                                    set_community,
-                                    function(err6, result3) {
-                                        if (err6) throw err6;
-                                        console.log("community file update");
-                                    }
-                                );
+                            for (j = 0; j < design.floorplan.walls.length; j++) {
+                                Group_obj.floorplan.walls.push(design.floorplan.walls[j])
+                            }
 
-                                db3.collection("community_info").updateOne(//更新料庫中community_info的update_time
-                                    myquery,
-                                    set_time,
-                                    function(err7, result4){
-                                        if (err7) throw err7;
-                                        console.log("community time update");
-                                    }
-                                )
-                                
-                                db3.collection("community_info").updateOne(//更新料庫中community_info
-                                    myquery,
-                                    set_xy,
-                                    function(err8, result5){
-                                        if (err8) throw err8;
-                                        console.log("corner info update");
-                                    }
-                                )
+                            // Add items
+                            for (j = 0; j < design.items.length; ++j) {
+                                design.items[j].xpos = design.items[j].xpos - rangeX
+                                design.items[j].zpos = design.items[j].zpos - rangeY
+                                design.items[j].item_name = user_name + "_" + design.items[j].item_name
+                            }
+                            for (j = 0; j < design.items.length; ++j) {
+                                Group_obj.items.push(design.items[j]);
+                            }
 
-                                db3.close()
-                            })//db3 connect
+                            communityX = communityX + (maxX - minX) + 200
+                            if ((maxY - minY) > communitymaxY)
+                                communitymaxY = maxY - minY
+
+                            if (communityX > 5000) {
+                                communityY = communityY + 600 + communitymaxY
+                                communityX = 0
+                                communitymaxY = 0
+                            }
+
                         }
+                        res.send({
+                            file: Group_obj
+                        })
                     })
                     db2.close()
                 })//db2 connect
